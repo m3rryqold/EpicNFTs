35a9b571f02b42dec7f010300ccd2cb97aced49c
Merrygold <merrymcx@gmail.com>
2022-04-25 22:28:06 +0100

    changed twitter link and started front-end validations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { ethers } from 'ethers';
 import myEpicNFT from './utils/MyEpicNFT.json';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'm3rryqold';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
@@ -154,7 +154,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
